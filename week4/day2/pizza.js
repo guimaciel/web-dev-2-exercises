f9a0901a4f83362414ce92189f4187185794773c
Guilherme Maciel de Almeida <guilherme@gmaciel.com>
2022-06-22 08:49:57 -0700

    .

--- week4/day2/pizza.js
+++ week4/day2/pizza.js
@@ -2,4 +2,19 @@ class Pizza {
   constructor() {
     this.toppings = ['cheese'];
   }
+
+  addTopping(newTopping) {
+    this.toppings.push(newTopping);
+  }
 }
+
+let pizza1 = new Pizza();
+console.log(pizza1.toppings); // ["cheese"]
+pizza1.addTopping('mushrooms');
+pizza1.addTopping('peppers');
+console.log(pizza1.toppings); // ["cheese", "mushrooms", "peppers"]
+
+let pizza2 = new Pizza();
+console.log(pizza2.toppings); // ["cheese"]
+pizza2.addTopping('more cheese');
+console.log(pizza2.toppings); // ["cheese", "more cheese"];
